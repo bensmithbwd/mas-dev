d9c158a69fbc1c4d7091eaf31fa711d27678b86d
Ben <bensmithbwd@gmail.com>
2013-03-25 15:55:05 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makeasplash::Application.config.secret_token = '195167ca3d5b239ef947ed5d574d5559208366a37a5e5e66c8ba08a2d98b1be984d2c73594e3151270cd5725f1842ab2727590772c8473480e8f507b09f00863'
